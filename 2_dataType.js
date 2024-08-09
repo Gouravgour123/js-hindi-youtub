6e0c2e7fd98e3b885fa878d67f586e56d2a75430
Gouravgour123 <gourg1899@gmail.com>
2024-08-09 23:22:13 +0530

    datatype

--- 2_dataType.js
+++ 2_dataType.js
@@ -11,11 +11,11 @@ let age = 24;
 let isLoggedIn = false;
 let state;
 
-console.log(`My name is ${name} and I am ${age} years old. I am logged in: ${isLoggedIn}`);
+console.log(`My name is ${name} and I am ${age} years old. I am logged in: ${isLoggedIn}`);                                                                    
 
 
 // number  => 2 to power 53
-// bigint
+// bigint                                             
 // string => ""
 // boolean => true/false
 // null => standalone value
