088f082b1a1462bcb9ded10abedf8041e268c1f2
Gouravgour123 <gourg1899@gmail.com>
2024-08-09 19:11:40 +0530

    datatype

---
+++ 2_dataType.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer v ersion
+
+// alert("hello") // ew are using nodejs  not browser
+
+console.log(3
+    +3)  // code readability should be high
+console.log("hello")
+
+let name = "gourav";
+let age = 24;
+let isLoggedIn = false;
+let state;
+
+console.log(`My name is ${name} and I am ${age} years old. I am logged in: ${isLoggedIn}`);
+
+
+// number  => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine =>
+// symbol  => unique 
+
+// object 
+
+console.log(typeof null) // object
+console.log(typeof undefined)  // undefined
---
+++ variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "gourav@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+accountEmail = "gfytfytfd"
+accountPassword = "6545"
+accountCity = "bhopal"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity ])
