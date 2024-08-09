0c93b151ad75261d6a57b635b33b1cf1dd9fa32f
Gouravgour123 <gourg1899@gmail.com>
2024-08-09 23:38:24 +0530

    conversion

---
+++ 3_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = undefined
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "33dtrd" => NaN
+// true => 1; false => 0;
+
+
+let isLoggedIn = "ju"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log( booleanIsLoggedIn)
+
+// 1 => true;  0 => false;
+// "" => false
+// "jhfg" => true
+
+
+let  someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
